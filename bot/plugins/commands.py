366ca43c572da2ae1b13751a4214727a1f1ef10e
autofilte <88162453+autofilte@users.noreply.github.com>
2021-07-30 01:37:41 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = f"{file_name} \n @boxchannellinks",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Our Group', url="https://t.me/boxofficegroups"
                                 )
                         ]
                     ]
